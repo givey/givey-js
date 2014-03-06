a622445eaa086219d59ac6000b0cc0169b0a72f2
Marc Qualie <marc@marcqualie.com>
2014-03-06 13:55:09 +0000

    Allow API settings to be overrideen

--- src/givey.js
+++ src/givey.js
@@ -1,11 +1,15 @@
-window.GiveyApi = (function () {
+window.GiveyApi = (function (options) {
 
   var self = this;
+
+  var options = options || {};
+  var api_host = options.host || 'https://api.givey.com';
+  var api_version = options.version || 1;
 
   self.find = function(type, id) {
     return new RSVP.Promise(function (resolve, reject) {
       var resource = type.pluralize();
-      var url =  'https://givey-api-staging.herokuapp.com/v1/' + resource + '/' + id;
+      var url =  api_host + '/v' + api_version + '/' + resource + '/' + id;
       $.get(url).then(function (data) {
         var model = new App[type.capitalize()](data[type]);
         resolve(model);
